534fde3f0c46d38e1905c17de8c0e69832a9c408
Androz2091 <androz2091@gmail.com>
2020-12-28 12:57:40 +0100

    :bug: Some fixes

--- index.js
+++ index.js
@@ -3,22 +3,25 @@ require('dotenv').config();
 const express = require('express');
 const app = express();
 
+const { graphql } = require('@octokit/graphql');
 const graphqlWithAuth = graphql.defaults({
     headers: {
-        authorization: process.env.GITHUB_AUTH,
+        authorization: `token ${process.env.GITHUB_AUTH}`,
     },
 });
 
-app.get('/stats/:login', (req, res) => {
+app.get('/stats/:username', async (req, res) => {
 
-    const username = req.query.username;
+    res.setHeader('Access-Control-Allow-Origin', '*');
+
+    const username = req.params.username;
     if (!username) {
         return res.status(404).send({
             error: 'Invalid Request'
         })
     }
 
-    const data = graphqlWithAuth(`
+    const data = await graphqlWithAuth(`
         {
             repos: search(query: "user:${username} fork:false", type: REPOSITORY, first: 100) {
                 repositoryCount
@@ -51,6 +54,10 @@ app.get('/stats/:login', (req, res) => {
     `);
 
     console.log(data);
+
+    res.send({
+        data
+    })
 
 });
 
